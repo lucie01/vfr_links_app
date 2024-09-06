a57ba907bd45ac2ec318c9a5be8fea91b28a7edd
Luciene <lucienep8@gmail.com>
2024-09-06 22:08:33 +0100

    Firebase config

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ from firebase_admin import credentials, firestore, initialize_app
 # Firebase Initialization
 def init_firebase():
     firebase_certificate = os.getenv('FIREBASE_CERTIFICATE')
+    print('FIREBASE_CERTIFICATE', os.getenv('FIREBASE_CERTIFICATE'))
     cred = credentials.Certificate(firebase_certificate)  # Replace with your Firebase Admin SDK key file path
     initialize_app(cred)
     return firestore.client()
