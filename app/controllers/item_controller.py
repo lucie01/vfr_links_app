168e601e0bb59ccc4e389224711a1c5477cb2a78
Luciene <lucienep8@gmail.com>
2024-09-09 20:29:00 +0100

    Correcting the requirements file

--- app/controllers/item_controller.py
+++ app/controllers/item_controller.py
@@ -10,7 +10,7 @@ from flask_cors import CORS  # Import CORS
 class ItemController:
     def __init__(self):
         self.blueprint = Blueprint('items', __name__)
-        CORS(self.blueprint, resources={r"/items/*": {"origins": "https://vfr-links-995250867468.europe-west1.run.app/"}})  # Allow only specific origin
+        CORS(self.blueprint, resources={r"/items/*": {"origins": ["https://vfr-links-995250867468.europe-west1.run.app", "http://localhost:3000"]}})  # Allow only specific origin
         self.collection_name = 'links'  # Replace with your actual collection name
         self.collection_ref = firestore.client().collection(self.collection_name)
 
